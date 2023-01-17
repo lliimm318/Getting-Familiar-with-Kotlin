a97c2429e711afd47d2399b6511d9c4e4f53609a
임서영 <201415isy@dsm.hs.kr>
2023-01-17 09:34:38 +0900

    [ADD]

---
+++ src/test/kotlin/getting/familiar/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package getting.familiar.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
