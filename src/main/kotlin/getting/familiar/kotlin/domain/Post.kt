57462f29621351a6a3cf879718658773fbbd22ee
임서영 <201415isy@dsm.hs.kr>
2023-01-25 23:51:10 +0900

    [ADD] post entity

---
+++ src/main/kotlin/getting/familiar/kotlin/domain/Post.kt
@@ -0,0 +1,29 @@
+package getting.familiar.kotlin.domain
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import java.time.LocalDate
+
+@Entity(name = "post")
+class Post (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+
+    @Column(length = 30, nullable = false)
+    var title: String = "none",
+
+    @Column(columnDefinition = "TEXT", length = 1600, nullable = false)
+    var content: String = "none",
+
+    @CreatedDate
+    @Column(nullable = false)
+    var date: LocalDate = LocalDate.now(),
+
+    @Column(length = 30, nullable = false)
+    var writer: String = "none"
+
+) {
+
+}
