42eaf1625565e8126140a67b017f8085f6b7c9ad
임서영 <201415isy@dsm.hs.kr>
2023-01-31 13:30:05 +0900

    [ADD]

---
+++ src/main/kotlin/getting/familiar/kotlin/domain/PostRepository.kt
@@ -0,0 +1,13 @@
+package getting.familiar.kotlin.domain
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostRepository : CrudRepository<Post, Int> {
+
+    fun findAllByIdOrderByDateDesc(id: Int): Post
+
+    fun findAllByOrderByDateDesc()
+
+}
